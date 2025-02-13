254e061483e7860d8b1c5d2761e7ce3dda24797e
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-02-13 16:22:35 -0500

    ICM (EDIT): use basic neorg editing

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -1,19 +1,43 @@
 return {
-  "nvim-orgmode/orgmode",
-  event = "VeryLazy",
-  ft = { "org" },
-  config = function()
-    -- Setup orgmode
-    require("orgmode").setup({
-      org_agenda_files = "~/orgfiles/**/*",
-      org_default_notes_file = "~/orgfiles/refile.org",
-    })
+  -- OPTION 1: much more emacs-like
 
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    -- require("nvim-treesitter.configs").setup({
-    --   ensure_installed = "all",
-    --   ignore_install = { "org" },
-    -- })
-  end,
+  -- "nvim-orgmode/orgmode",
+  -- event = "VeryLazy",
+  -- ft = { "org" },
+  -- config = function()
+  --   -- Setup orgmode
+  --   require("orgmode").setup({
+  --     org_agenda_files = "~/orgfiles/**/*",
+  --     org_default_notes_file = "~/orgfiles/refile.org",
+  --   })
+  --
+  --   -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+  --   -- add ~org~ to ignore_install
+  --   -- require("nvim-treesitter.configs").setup({
+  --   --   ensure_installed = "all",
+  --   --   ignore_install = { "org" },
+  --   -- })
+  -- end,
+
+  -- OPTION 2: new thing
+
+  "nvim-neorg/neorg",
+  -- build = ":Neorg sync-parsers",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.integrations.treesitter"] = {}, -- Adds pretty icons to your documents
+      ["core.qol.todo_items"] = {},
+      ["core.ui.calendar"] = {},
+      -- ["core.dirman"] = { -- Manages Neorg workspaces
+      --   -- config = {
+      --   --   workspaces = {
+      --   --     notes = "~/notes",
+      --   --   },
+      --   -- },
+      -- },
+    },
+  },
+  dependencies = { { "nvim-lua/plenary.nvim" } },
 }

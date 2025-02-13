bcbb76f9adae7881be3a5cd4f0d5689ace90dc54
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-02-13 15:40:28 -0500

    FEAT(EDIT): support inline markdown editing

--- nvim/lua/plugins/headlines.lua
+++ nvim/lua/plugins/headlines.lua
@@ -23,32 +23,36 @@ return {
     -- Setup headlines.nvim with the newly defined highlight groups
     require("headlines").setup({
       markdown = {
-        -- If set to false, headlines will be a single line and there will be no
-        -- "fat_headline_upper_string" and no "fat_headline_lower_string"
-        fat_headlines = true,
-        --
-        -- Lines added above and below the header line makes it look thicker
-        -- "lower half block" unicode symbol hex:2584
-        -- "upper half block" unicode symbol hex:2580
-        fat_headline_upper_string = "▄",
-        fat_headline_lower_string = "▀",
-        --
-        -- You could add a full block if you really like it thick ;)
-        -- fat_headline_upper_string = "█",
-        -- fat_headline_lower_string = "█",
-        --
-        -- Other set of lower and upper symbols to try
-        -- fat_headline_upper_string = "▃",
-        -- fat_headline_lower_string = "-",
-        --
-        headline_highlights = {
-          "Headline1",
-          "Headline2",
-          "Headline3",
-          "Headline4",
-          "Headline5",
-          "Headline6",
-        },
+        -- OPTION 1: with markdown.lua
+        headline_highlights = false,
+
+        -- OPTION 2: without markdown.lua
+        -- -- If set to false, headlines will be a single line and there will be no
+        -- -- "fat_headline_upper_string" and no "fat_headline_lower_string"
+        -- fat_headlines = true,
+        -- --
+        -- -- Lines added above and below the header line makes it look thicker
+        -- -- "lower half block" unicode symbol hex:2584
+        -- -- "upper half block" unicode symbol hex:2580
+        -- fat_headline_upper_string = "▄",
+        -- fat_headline_lower_string = "▀",
+        -- --
+        -- -- You could add a full block if you really like it thick ;)
+        -- -- fat_headline_upper_string = "█",
+        -- -- fat_headline_lower_string = "█",
+        -- --
+        -- -- Other set of lower and upper symbols to try
+        -- -- fat_headline_upper_string = "▃",
+        -- -- fat_headline_lower_string = "-",
+        -- --
+        -- headline_highlights = {
+        --   "Headline1",
+        --   "Headline2",
+        --   "Headline3",
+        --   "Headline4",
+        --   "Headline5",
+        --   "Headline6",
+        -- },
       },
     })
   end,
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,9 +1,45 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
+  -- "MeanderingProgrammer/render-markdown.nvim",
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  -- -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  -- ---@module 'render-markdown'
+  -- ---@type render.md.UserConfig
+  -- opts = {},
+
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  ft = { "markdown", "markdown-inline" },
+  latex = {
+    enable = true,
+    inlines = { "$" },
+  },
   opts = {},
+  config = function()
+    local preset = require("markview.presets").headings
+    require("markview").setup({
+      markdown = {
+        headings = {
+          enable = true,
+          shift_width = 0,
+          heading_1 = {
+            style = "label",
+            align = "left",
+            icon = "+",
+            sign = "++",
+            corner_left = " ░▒▓",
+            padding_left = " ",
+            corner_left_hl = "@comment.warning",
+          },
+        },
+        -- tables = preset.single,
+      },
+      latex = {
+        enable = true,
+      },
+    })
+  end,
+
+  -- needs hologram; kitty/linux + macos based
+  -- "Vaisakhkm2625/hologram-math-preview.nvim",
 }
